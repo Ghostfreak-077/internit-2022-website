5995f32ff32d2bc87da36c15a27d8aab94101b71
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2022-10-26 01:23:07 +0530

    First Commit

---
+++ src/components/Home/Images.js
@@ -0,0 +1,25 @@
+import Picture from '../Assets_Images/Picture.png'
+import Picture2 from '../Assets_Images/Picture2.png'
+import Picture3 from '../Assets_Images/Picture3.png'
+
+const images = [{
+    id: 1,
+    src: `${Picture}`,
+    text:"Badminton",
+    alt: 'Badminton Image'
+    },
+    {
+        id: 2,
+        text:"Chess",
+        src: `${Picture2}`,
+        alt: 'Chess Image'        
+    },
+    {
+        id: 3,
+        text:"Table tennis",
+        src: `${Picture3}`,
+        alt: 'Table Tennis Image'
+    }    
+];
+
+export default images;
