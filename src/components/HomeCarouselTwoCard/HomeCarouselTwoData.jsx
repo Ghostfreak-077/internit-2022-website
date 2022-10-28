6ac93b64d9706d4eba1485436d682dcec46c5a0e
Rupsnigdha <83987752+Rupsnigdha@users.noreply.github.com>
2022-10-28 22:29:55 +0530

    ADD: Carousels for home

---
+++ src/components/HomeCarouselTwoCard/HomeCarouselTwoData.jsx
@@ -0,0 +1,24 @@
+import Previous1 from "../../assets/images/Home/Previous1.png";
+import Previous2 from "../../assets/images/Home/Previous2.png";
+import Previous3 from "../../assets/images/Home/Previous3.png";
+
+const HomeCarouselTwoData = [
+  {
+    id: 1,
+    src: `${Previous1}`,
+  },
+  {
+    id: 2,
+    src: `${Previous2}`,
+  },
+  {
+    id: 3,
+    src: `${Previous3}`,
+  },
+  {
+    id: 4,
+    src: `${Previous3}`,
+  },
+];
+
+export default HomeCarouselTwoData;
---
+++ src/components/HomeUpcomingCard/HomeUpcomingCardData.jsx
@@ -0,0 +1,23 @@
+import UpcomingBadminton from "../../assets/images/Home/UpcomingBadminton.png";
+import UpcomingChess from "../../assets/images/Home/UpcomingChess.png";
+import UpcomingTT from "../../assets/images/Home/UpcomingTT.png";
+
+const HomeUpcomingCardData = [
+  {
+    id: 1,
+    name: "Badminton",
+    src: `${UpcomingBadminton}`,
+  },
+  {
+    id: 2,
+    name: "Chess",
+    src: `${UpcomingChess}`,
+  },
+  {
+    id: 3,
+    name: "Table Tennis",
+    src: `${UpcomingTT}`,
+  },
+];
+
+export default HomeUpcomingCardData;
