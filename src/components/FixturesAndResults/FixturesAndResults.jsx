f327b7f218fc375bd948b90645cc768e1f27c9c0
Rayyan Shaikh <rayyan.shaikh01@gmail.com>
2022-10-30 04:37:55 +0530

    rayyan completed

--- src/components/FixturesAndResults/FixturesAndResults.jsx
+++ src/components/FixturesAndResults/FixturesAndResults.jsx
@@ -1,54 +1,89 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ScoreCard from "../ScoreCard.jsx";
 import './fixturesAndResults.css'
 
 export default function FixturesAndResults() {
+
+    const dummyData=[
+        {
+            sport: "Chess",
+            status: "Upcoming",
+            time: "14:50",
+            date: "22nd November, 2022",
+            team1: "NIT Silchar", 
+            team2: "NIT Patna", 
+            team1score: 2,
+            team2score: 0,
+            image: "link",
+            addInfo: "Base of 3" 
+        },
+        {
+            sport: "Chess",
+            status: "Completed",
+            time: "14:50",
+            date: "22nd November, 2022",
+            team1: "NIT Silchar", 
+            team2: "NIT Patna", 
+            team1score: 2,
+            team2score: 3,
+            image: "link",
+            addInfo: "Base of 3" 
+        },
+        {
+            sport: "Chess",
+            status: "Upcoming",
+            time: "14:50",
+            date: "22nd November, 2022",
+            team1: "NIT Silchar", 
+            team2: "NIT Patna", 
+            team1score: 2,
+            team2score: 0,
+            image: "link",
+            addInfo: "Base of 3" 
+        },
+
+    ]
+
+    const [sport, setSport] = useState("Badminton");
+    const [sortBy, setSortBy] = useState("Upcoming");
+    const [click, setClick] = useState(0);
+
+    function handleClick(){
+        if(sortBy==="Upcoming"){
+            setSortBy("Completed")
+        }else{
+            setSortBy("Upcoming")
+        }
+        setClick(0);
+    }
+
     return (
         <div>
             <div className="sport-categories">
                 <ul className="sport-cat-container">
-                    <li>BADMINTON</li>
-                    <li>CHESS</li>
-                    <li>TABLE TENNIS</li>
+                    <li value="Badminton" onClick={() => setSport("Badminton")} className={sport==="Badminton" && "selected"}>BADMINTON</li>
+                    <li value="Chess" onClick={() => setSport("Chess")} className={sport==="Chess" && "selected"}>CHESS</li>
+                    <li value="Table Tennis" onClick={() => setSport("Table Tennis")} className={sport==="Table Tennis" && "selected"}>TABLE TENNIS</li>
                 </ul>
             </div>
             <div className="sort-cat">
-                <span className="sort-by">Sort by : &#12644;&#12644;&#12644;</span>
-                {/* <span className="dropdown">
-                    <button className="sortBy">
-                        Dropdown
-                    </button>
-                    <ul className="sort-rodeo">
-                        <li><button className="dropdown-item">Dropdown item</button></li>
-                        <li><button className="dropdown-item">Dropdown item</button></li>
-                    </ul>
-                </span> */}
-                <div class="dropdown">
-                    <button class="dropbtn" id="myColors">Upcoming <img src={require('./down.png')} alt="down" /></button>
-                    <div class="dropdown-content">
-                        <a href="#" value="Completed">Completed</a>
-                        <a href="#" value="Upcoming">Upcoming</a>
+                <span className="sort-by">Sort by : </span>
+                <div className="dropdown">
+                    <button className="dropbtn" onClick={() => setClick(1)}>{sortBy}<img src={require('./down.png')} alt="down" className={click && "invert"} /></button>
+                    <div className={`dropdown-content ${click && "show"}`}>
+                        <a value="Completed" onClick={() => handleClick()}>{sortBy==="Upcoming"?"Completed":"Upcoming"}</a>
                     </div>
                 </div>
             </div>
             <div className="scores">
-                <ScoreCard
-                    color="darkgray"
-                />
-                <ScoreCard
-                    color="red"
-                />
-                <ScoreCard
-                    color="red"
-                />
-                <ScoreCard
-                    color="red"
-                />
-                <ScoreCard
-                    color="red"
-                />
-            </div>
+                {dummyData.map((elem, i) => 
+                (<ScoreCard
+                    key={i}
+                    data={elem}
+                />)
 
+                )}
+            </div>
         </div>
     )
 }
--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,37 +1,37 @@
 import React from 'react'
 
-const ScoreCard = ({color}) => {
+const ScoreCard = ({data}) => {
   return (
-    <div className="score-container" style={{borderTop: `1px solid ${color?color:"gray"}`}}>
+    <div className="score-container" style={{borderTop: `1px solid ${data.status==="Upcoming"?"red":"gray"}`}}>
         <span className="match-title">Match 1 - Completed</span>
         <div className="match-details">
             <span className="match-time">
-                Time :- 16:50
+                Time :- {data.time}
             </span> | <span className="match-date">
-                Date :- Friday, 07 October, 2022
+                Date :- {data.date}
             </span>
         </div>
         <div className="team1">
-            <span className="team-name1">NIT Silchar</span>
-            <img src="https://picsum.photos/200" alt="team-logo" className="team-logo" />      
+            <span className="team-name1">{data.team1}</span>
+            <img src={"https://picsum.photos/200" || data.link} alt="team-logo" className="team-logo" />      
         </div>
         <div className="score-box">
             <div className="score">
-                2
+                {data.team1score}
             </div>
             <div className="vs">
                 - VS -
             </div>
             <div className="score">
-                0
+                {data.team2score}
             </div>
         </div>
         <div className="team2">
             <img src="https://picsum.photos/200" alt="team-logo" className="team-logo" />      
-            <span className="team-name2">NIT Meghalaya</span>         
+            <span className="team-name2">{data.team2}</span>         
         </div>
         <div className="add-info">
-            Base of 3
+            {data.addInfo}
         </div>
     </div>
   )
