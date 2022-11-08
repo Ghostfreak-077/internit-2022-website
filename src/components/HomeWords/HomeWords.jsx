d2882c3225a43ddd331b903237fca0616196a5aa
Rayyan Shaikh <rayyan.shaikh01@gmail.com>
2022-11-08 15:07:35 +0530

    some words section  updated

--- src/components/HomeWords/HomeWords.jsx
+++ src/components/HomeWords/HomeWords.jsx
@@ -7,28 +7,31 @@ class HomeWords extends Component {
     this.state = HomeWordsData;
     return (
       <div className="home-words-container container">
-        <h2 className="home-words-heading">Some words from</h2>
-        <h2 className="home-words-heading my-2">Our team heads</h2>
+        {/* <h2 className="home-words-heading">Some words from</h2>
+        <h2 className="home-words-heading my-2">Our team heads</h2> */}
         {this.state.map((wordInfo) => {
           return (
-            <div className={`home-words-content-container container ${wordInfo.id===2?'flex-row-reverse':''}`}>
-              <div className="home-words-content-person">
-                <h3 className="home-words-content-person-desk">
-                  From the {wordInfo.desg}'s desk
-                </h3>
-                <img
-                  src={wordInfo.src}
-                  alt=""
-                  className="home-words-content-img"
-                />
-                <h3 className="home-words-content-person-name">
-                  {wordInfo.name}
-                </h3>
-                <h4 className="home-words-content-person-desg">
-                  {wordInfo.desg}
-                </h4>
+            <div className={`home-words-content-container flex-col-r container `}>
+              <h3 className="home-words-content-person-desk home-words-title">
+                From the {wordInfo.desg}'s desk
+              </h3>
+              <div className={`home-words-content-container ${wordInfo.id === 2 ? 'flex-row-reverse' : ''}`}>
+
+                <div className="home-words-content-person">
+                  <img
+                    src={wordInfo.src}
+                    alt=""
+                    className="home-words-content-img"
+                  />
+                  <h3 className="home-words-content-person-name">
+                    {wordInfo.name}
+                  </h3>
+                  <h4 className="home-words-content-person-desg">
+                    {wordInfo.desg}
+                  </h4>
+                </div>
+                <p className="home-words-content-words">{wordInfo.text}</p>
               </div>
-              <p className="home-words-content-words">{wordInfo.text}</p>
             </div>
           );
         })}
